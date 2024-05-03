d4a9a3c33d6bb352e4a9250999443ed8e44973be
iladyka <dev.ivan.ladyka@gmail.com>
2024-05-03 20:01:04 +0300

    fix: adaptive fixed

--- src/components/TopMenu.vue
+++ src/components/TopMenu.vue
@@ -20,22 +20,24 @@ setInterval(() => {
 <template>
     <header class="shadow">
 			<div class="container">
-				<nav class="navbar navbar-expand-lg navbar-light px-5 py-4">
-					<router-link to="/" class="navbar-brand mr-5" style="margin-right: 40px;">
-						<img src="../assets/vue.svg" width="30" height="30" class="d-inline-block align-top" alt="">
-						INVENTORY
-					</router-link>
-					<div class="flex-grow-1">
-						<form class="d-flex w-100 w-xl-25" role="search">
-							<input class="form-control me-2 shadow-none border-secondary" type="search" placeholder="Search" aria-label="Search" style="max-width: 300px;">
-						</form>
-					</div>
-					<div>
-						<div class="d-flex flex-column align-items-start">
-							<div>Today</div>
-							<div class="d-flex flex-row">
-								<div class="me-3">{{ currentDay }}</div>
-								<div class="d-flex align-items-center"><ClockIcon style="width: 15px;" /> {{ currentTime }}</div>
+				<nav class="navbar px-md-1 py-md-4 py-3">
+					<div class="container-fluid">
+						<div class="d-flex">
+							<router-link to="/" class="navbar-brand me-5" style="margin-right: 40px;">
+								<img src="../assets/vue.svg" width="30" height="30" class="d-inline-block align-top" alt="">
+								INVENTORY
+							</router-link>
+							<form class="d-flex" role="search">
+								<input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+							</form>
+						</div>
+						<div class="d-none d-md-block">
+							<div class="d-flex flex-column align-items-start">
+								<div>Today</div>
+								<div class="d-flex flex-row">
+									<div class="me-3">{{ currentDay }}</div>
+									<div class="d-flex align-items-center"><ClockIcon style="width: 15px;" /> {{ currentTime }}</div>
+								</div>
 							</div>
 						</div>
 					</div>
