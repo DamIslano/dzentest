961ee3beaab3899f31f2385ae0e47d6b823279d6
iladyka <dev.ivan.ladyka@gmail.com>
2024-05-11 12:12:13 +0300

    update: orders

--- src/views/orders/Orders.vue
+++ src/views/orders/Orders.vue
@@ -6,23 +6,31 @@ import Products from '../products/Products.vue';
 
 const { allOrders } = useStore().getters
 const fullMenu = ref(true)
+const openedOrderIndex = ref(-1)
 
-const toggleMenu = () => fullMenu.value = !fullMenu.value
+const toggleMenu = (index: number) => {
+	if(!fullMenu.value && openedOrderIndex.value != index) {
+		openedOrderIndex.value = index
+	} else {
+		fullMenu.value = !fullMenu.value
+		openedOrderIndex.value = index 
+	}
+}
 </script>
 
 <template>
 	<div :class="{ 'd-flex alight-items-center justify-content-between': !fullMenu }">
 		<div :style="{ 'min-width': fullMenu ? '' : '30%' }">
 			<div 
-				v-for="order in allOrders" 
+				v-for="(order, idx) in allOrders" 
 				class="p-2 mb-3 card position-relative"
 				:class="{ 'px-3': !fullMenu, 'px-5': fullMenu }" 
 				:style="{ width: fullMenu ? '' : '100%' }"
 			>
 				<div class="d-flex align-items-center justify-content-between py-1">
-					<div v-if="fullMenu">{{ order.order_name }}</div>
+					<div v-if="fullMenu" style="max-width: 180px; width: 100%;">{{ order.order_name }}</div>
 					<div class="d-flex align-items-center">
-						<div @click="toggleMenu" class="me-2 d-flex align-items-center justify-content-center rounded-circle border order-list-icon"><ListBulletIcon style="width: 20px;"/></div>
+						<div @click="toggleMenu(idx)" class="me-2 d-flex align-items-center justify-content-center rounded-circle border order-list-icon"><ListBulletIcon style="width: 20px;"/></div>
 						<div>
 							<div>{{ order.product_amount }}</div>
 							<div>Products</div>
@@ -32,12 +40,12 @@ const toggleMenu = () => fullMenu.value = !fullMenu.value
 						<div>{{ order.creation_date.short }}</div>
 						<div>{{ order.creation_date.full }}</div>
 					</div>
-					<div v-if="fullMenu">
+					<div v-if="fullMenu" style="min-width: 120px;">
 						<div>{{ order.price.USD }} $</div>
 						<div>{{ order.price.UAH }} UAH</div>
 					</div>
 					<div v-if="fullMenu" class="me-4 hover-overlay d-flex alighn-items-center justify-content-center rounded-circle delete-icon" style="min-width: 50px; height: 50px;"><TrashIcon style="width: 20px;"/></div>
-					<div v-if="!fullMenu" class="position-absolute end-0 h-100 d-flex alighn-items-center justify-content-center order-opened"><ChevronRightIcon style="width: 25px; color: white;"/></div>
+					<div v-show="openedOrderIndex === idx && !fullMenu" class="position-absolute end-0 h-100 d-flex alighn-items-center justify-content-center order-opened" :class="{ active: openedOrderIndex === idx && !fullMenu }"><ChevronRightIcon style="width: 25px; color: white;"/></div>
 				</div>
 			</div>
 		</div>
